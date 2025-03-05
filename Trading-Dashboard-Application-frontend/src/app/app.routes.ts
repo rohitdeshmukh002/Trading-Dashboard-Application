58d4ee944cd722bd14e0d33787036111e185243d
rohitdeshmukh002 <rohitdeshmukh649@gmail.com>
2025-03-05 12:53:48 +0530

    added two component with routing

--- Trading-Dashboard-Application-frontend/src/app/app.routes.ts
+++ Trading-Dashboard-Application-frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { ChartsComponent } from './features/charts/charts/charts.component';
+import { HomeComponent } from './features/homeLayout/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path : ' ',
+        component : ChartsComponent,
+        children :[
+            {
+                path : '',
+                redirectTo: '/home',
+                pathMatch: 'full'
+            },
+            {
+                path: 'home',
+                component : HomeComponent
+            }
+        ]
+
+    }
+];
